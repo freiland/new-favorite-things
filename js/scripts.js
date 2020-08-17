86c5f0d465c043d8f3b2d378db937be5d6bb1fb6
Forrest Reiland <you@example.com>
2020-08-17 11:00:37 -0700

    add feature to script to return only the first two items of favorite things list

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("form#fav").submit(function (event) {
+    event.preventDefault();
+    const favFood = $("input#favoritefood").val();
+    const favColor = $("input#favoritecolor").val();
+    const favMovie = $("input#favoritemovie").val();
+    let favorites = [favFood, favColor, favMovie];
+    let newFave = favorites.pop();
+    const favOne = favorites[0];
+    const favTwo = favorites[1];
+    alert(favOne);
+    alert(favTwo);
+    $("ul").append("<li>" + favOne + "</li>");
+    $("ul").append("<li>" + favTwo + "</li>");
+
+  });
+});
